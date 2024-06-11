fb9dcb43830a448dd9ee04038f98a4c7f5f6ffa9
Ad <me@skissue.xyz>
2024-06-11 10:47:36 -0400

    docs: complete commentary in package file

--- meow-tree-sitter.el
+++ meow-tree-sitter.el
@@ -26,7 +26,22 @@
 
 ;;; Commentary:
 
-;; (WIP) Tree-sitter powered motions for Meow
+;; 'meow-tree-sitter' is a package that integrates the treesit library in Emacs
+;; 29+ 🌳 with Meow’s motions 🐱. Lots of functionality is ported from
+;; 'evil-textobj-tree-sitter'.
+
+;; To get started, call `meow-tree-sitter-register-defaults' to add the default
+;; keybinds to Meow's "thing" registry. They will now be accessible through
+;; \\[meow-bounds-of-thing] and \\[meow-inner-of-thing], as well as
+;; \\[meow-beginning-of-thing] and \\[meow-end-of-thing].
+
+;; Evaluate (customize-group 'meow-tree-sitter) to see customization options.
+;; Options are also detailed in the README.org file included with this package.
+
+;; To register custom keybinds, call `meow-tree-sitter-register-thing' with a
+;; key and type (or list of types). To register custom queries, pass the
+;; optional third argument. Persistent queries can be registered in
+;; `meow-tree-sitter-extra-queries'. See the README.org file for details.
 
 ;;; Code:
 
